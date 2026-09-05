c43950317418a7fbf5aef356a47f6506ce24137c
Abhinav Pratap <pratapabhinav275@gmail.com>
2026-09-06 01:02:43 +0530

    Updated solution

--- easy/DividingOrange_244A/solution.py
+++ easy/DividingOrange_244A/solution.py
@@ -1,30 +1,29 @@
 n, k = map(int, input().split())
 a = list(map(int, input().split()))
 
-# Store (requested_segment, child_index)
 children = [(a[i], i) for i in range(k)]
-
-# Process requested segments from largest to smallest
 children.sort(reverse=True)
+
+requested = set(a)
 
 used = [False] * (n * k + 1)
 ans = [[] for _ in range(k)]
 
+current = n * k
+
 for value, child in children:
-    # Give the requested segment
+    # Give requested segment
     ans[child].append(value)
     used[value] = True
 
-    # Give n-1 unused smaller segments
-    current = value - 1
-
+    # Give remaining n-1 segments
     while len(ans[child]) < n:
-        if not used[current]:
-            ans[child].append(current)
-            used[current] = True
+        while used[current] or current in requested:
+            current -= 1
 
+        ans[child].append(current)
+        used[current] = True
         current -= 1
 
-# Print answer
 for child in range(k):
     print(*ans[child])
